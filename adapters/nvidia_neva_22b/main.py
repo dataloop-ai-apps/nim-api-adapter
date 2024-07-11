222c585f54237bb04f9a3aa79a647aa7729477ab
ShadiMah <shadi.m@dataloop.ai>
2024-07-11 11:58:10 +0300

    commit message

---
+++ adapters/nvidia_neva_22b/main.py
@@ -0,0 +1,100 @@
+import os
+
+import requests, base64
+import json
+import dtlpy as dl
+import logging
+
+logger = logging.getLogger("NIM Adapter")
+
+
+class ModelAdapter(dl.BaseModelAdapter):
+    def __init__(self, model_entity: dl.Model, nvidia_api_key_name=None):
+        super().__init__(model_entity)
+        # with open('.env') as f:
+        #     params = json.load(f)
+        api_key = os.environ.get(nvidia_api_key_name, None)
+        if api_key is None:
+            raise ValueError(f"Missing API key: {nvidia_api_key_name}")
+        self.invoke_url = "https://ai.api.nvidia.com/v1/vlm/nvidia/neva-22b"
+        self.headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Accept": "application/json"
+        }
+        self.max_token = self.model_entity.configuration.get('max_token', 1024)
+        self.temperature = self.model_entity.configuration.get('temperature', 0.2)
+        self.top_p = self.model_entity.configuration.get('top_p', 0.7)
+        self.seed = self.model_entity.configuration.get('seed', 0)
+
+    def load(self, local_path, **kwargs):
+        pass
+
+    def prepare_item_func(self, item: dl.Item):
+        if 'json' not in item.mimetype or item.metadata.get('system', dict()).get('shebang', dict()).get(
+                'dltype') != 'prompt':
+            logger.warning(f"Item is not a JSON file or a Prompt item.")
+            return None
+        buffer = json.load(item.download(save_locally=False))
+        return buffer
+
+    def predict(self, batch, **kwargs):
+        annotations = []
+        for prompt_item in batch:
+            ann_collection = dl.AnnotationCollection()
+            for prompt_name, prompt_content in prompt_item.get('prompts').items():
+                text = None
+                encoded_image = None
+                for partial_prompt in prompt_content:
+                    if 'image' in partial_prompt.get('mimetype', ''):
+                        image_url = partial_prompt.get('value', '')
+                        item_id = image_url.split("/stream")[0].split("/items/")[-1]
+                        image_buffer = dl.items.get(item_id=item_id).download(save_locally=False).getvalue()
+                        encoded_image = base64.b64encode(image_buffer).decode()
+
+                        # Encoding testing
+                        # decoded_bytes = base64.b64decode(encoded_image)
+                        # Open a file for writing in binary mode and write the decoded bytes
+                        # with open("image.jpg", "wb") as image_file:
+                        #     image_file.write(decoded_bytes)
+
+                    elif 'text' in partial_prompt.get('mimetype', ''):
+                        text = partial_prompt.get('value')
+                    else:
+                        logger.warning(
+                            f"Prompt from type {partial_prompt.get('mimetype', '')} is not supported either an image or a text prompt.")
+                if text is None or encoded_image is None:
+                    logger.warning(f"{prompt_name} is missing either an image or a text prompt.")
+                    continue
+                payload = {
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": f'{text}. <img src="data:image/png;base64,{encoded_image}" />'
+                        }
+                    ],
+                    "max_tokens": self.max_token,
+                    "temperature": self.temperature,
+                    "top_p": self.top_p,
+                    "seed": self.seed,
+                    "stream": False
+                }
+                response = requests.post(self.invoke_url, headers=self.headers, json=payload)
+                content = response.json().get('choices')[0].get('message').get('content')
+                ann_collection.add(
+                    annotation_definition=dl.FreeText(text=content),
+                    prompt_id=prompt_name,
+                    model_info={
+                        'name': self.model_entity.name,
+                        'model_id': self.model_entity.id,
+                        'confidence': 1.0
+                    }
+                )
+            annotations.append(ann_collection)
+        return annotations
+
+
+if __name__ == '__main__':
+    model = dl.models.get(model_id='')
+    item = dl.items.get(item_id='')
+    adapter = ModelAdapter(model)
+    adapter.predict_items(items=[item])
---
+++ main_adapter.py
@@ -0,0 +1,115 @@
+from openai import OpenAI
+import dtlpy as dl
+import requests
+import logging
+import json
+import os
+
+logger = logging.getLogger("NIM Adapter")
+
+
+class ModelAdapter(dl.BaseModelAdapter):
+    def __init__(self, model_entity: dl.Model):
+        if os.environ.get("NVIDIA_NIM_API_KEY", None) is None:
+            raise ValueError(f"Missing API key: NVIDIA_NIM_API_KEY")
+        super().__init__(model_entity)
+
+    def load(self, local_path, **kwargs):
+        self.nim_model_name = self.configuration.get("nim_model_name")
+        if self.nim_model_name is None:
+            raise ValueError("Missing `nim_model_name` from model.configuration, cant load the model without it")
+
+    def prepare_item_func(self, item: dl.Item):
+        if ('json' not in item.mimetype or
+                item.metadata.get('system', dict()).get('shebang', dict()).get('dltype') != 'prompt'):
+            raise ValueError('Only prompt items are supported')
+        buffer = json.load(item.download(save_locally=False))
+        return buffer
+
+    def call_model_open_ai(self, messages):
+        client = OpenAI(
+            base_url="https://integrate.api.nvidia.com/v1",
+            api_key=os.environ.get("NVIDIA_NIM_API_KEY", None)
+        )
+        completion = client.chat.completions.create(
+            model=self.nim_model_name,
+            messages=messages,
+            temperature=0.5,
+            top_p=1,
+            max_tokens=1024,
+            stream=True
+        )
+        full_answer = ""
+        for chunk in completion:
+            if chunk.choices[0].delta.content is not None:
+                full_answer += chunk.choices[0].delta.content
+        return full_answer
+
+    def call_model_requests(self, messages):
+        url = f"https://ai.api.nvidia.com/v1/{self.model_entity.configuration.get('nim_model_name')}"
+        headers = {
+            "Authorization": f"Bearer {os.environ.get('NVIDIA_NIM_API_KEY', None)}",
+            "Accept": "application/json"
+        }
+        max_token = self.model_entity.configuration.get('max_token', 1024)
+        temperature = self.model_entity.configuration.get('temperature', 0.2)
+        top_p = self.model_entity.configuration.get('top_p', 0.7)
+        seed = self.model_entity.configuration.get('seed', 0)
+
+        payload = {
+            "messages": messages,
+            "max_tokens": max_token,
+            "temperature": temperature,
+            "top_p": top_p,
+            "seed": seed,
+            "stream": False
+        }
+        response = requests.post(url=url, headers=headers, json=payload)
+        full_answer = response.json().get('choices')[0].get('message').get('content')
+        return full_answer
+
+    def predict(self, batch, **kwargs):
+        system_prompt = self.model_entity.configuration.get('system_prompt', None)
+
+        annotations = []
+        for prompt_item in batch:
+            ann_collection = dl.AnnotationCollection()
+            for prompt_name, prompt_content in prompt_item.get('prompts').items():
+                # get latest question
+                question = [p['value'] for p in prompt_content if 'text' in p['mimetype']][0]
+                nearest_items = [p['nearestItems'] for p in prompt_content if 'metadata' in p['mimetype']][0]
+                # build context
+                context = ""
+                for item_id in nearest_items:
+                    context_item = dl.items.get(item_id=item_id)
+                    with open(context_item.download(), 'r', encoding='utf-8') as f:
+                        text = f.read()
+                    context += f"\n{text}"
+                messages = [{"role": "system",
+                             "content": system_prompt},
+                            {"role": "assistant",
+                             "content": context},
+                            {"role": "user",
+                             "content": question}]
+                if self.nim_model_name.startswith('vlm/'):
+                    full_answer = self.call_model_requests(messages=messages)
+                else:
+                    full_answer = self.call_model_open_ai(messages=messages)
+                ann_collection.add(
+                    annotation_definition=dl.FreeText(text=full_answer),
+                    prompt_id=prompt_name,
+                    model_info={
+                        'name': self.model_entity.name,
+                        'model_id': self.model_entity.id,
+                        'confidence': 1.0
+                    }
+                )
+            annotations.append(ann_collection)
+        return annotations
+
+
+if __name__ == '__main__':
+    model = dl.models.get(model_id='')
+    item = dl.items.get(item_id='')
+    adapter = ModelAdapter(model)
+    adapter.predict_items(items=[item])
