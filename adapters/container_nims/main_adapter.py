852525c4d1ba6c8f5352132006ceed54e1f0c9eb
ShadiMah <shadi.m@dataloop.ai>
2024-07-30 12:14:09 +0300

    adding container adapters

---
+++ adapters/container_nims/main_adapter.py
@@ -0,0 +1,23 @@
+import dtlpy as dl
+import subprocess
+import logging
+import time
+from openai import OpenAI
+
+logger = logging.getLogger('NiM-Model')
+
+
+class ModelAdapter(dl.BaseModelAdapter):
+    def load(self, local_path, **kwargs):
+        run_api_server_command = 'bash /opt/nim/start-server.sh'
+        run_api_server = subprocess.Popen(run_api_server_command,
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE,
+                                          shell=True)
+        while run_api_server.poll() is None:
+            time.sleep(120)
+
+        (out, err) = run_api_server.communicate()
+        if run_api_server.returncode != 0:
+            raise Exception(f'Failed to start API server: {err}')
+        self.client = OpenAI(base_url='http://0.0.0.0:8000/v1', api_key="not=used")
